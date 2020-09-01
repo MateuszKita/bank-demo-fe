b456e42574cea6ed38b5da67eeef42598de2fd84
MateuszKita <mkita.zg@gmail.com>
2020-09-01 21:44:16 +0200

    create component for masked password

--- src/app/modules/entry/login/login-step-two/masked-password/masked-password.component.ts
+++ src/app/modules/entry/login/login-step-two/masked-password/masked-password.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {MaskedPasswordChar} from "./masked-password.model";
+import {MASKED_CHARS_LENGTH} from "./masked-password.constants";
 
 @Component({
   selector: 'bd-masked-password',
@@ -7,10 +9,52 @@ import {Component, OnInit} from '@angular/core';
 })
 export class MaskedPasswordComponent implements OnInit {
 
+  private enabledIndexes: number[] = [1, 5, 6, 9, 12, 13];
+
+  public chars: MaskedPasswordChar[];
+
   constructor() {
   }
 
   ngOnInit(): void {
+    this.chars = this.createChars(this.enabledIndexes, MASKED_CHARS_LENGTH);
+  }
+
+  private createChars(enabledIndexes: number[], passwordLength: number): MaskedPasswordChar[] {
+    const chars: MaskedPasswordChar[] = [];
+    for (let counter = 0; counter < passwordLength; counter++) {
+      const isCharInRandomIndex: boolean = enabledIndexes.includes(counter)
+      chars.push({disabled: !isCharInRandomIndex, value: isCharInRandomIndex ? "" : "x"})
+    }
+    return chars;
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    if (event.key === "ArrowLeft") {
+      this.goToInputSibling(false, event.target as HTMLInputElement);
+    } else if (event.key === "ArrowRight" || (event.target as HTMLInputElement).value.length > 0) {
+      this.goToInputSibling(true, event.target as HTMLInputElement);
+    }
+  }
+
+  private goToInputSibling(next: boolean, target: HTMLInputElement): void {
+    const siblingKey: string = next ? "nextSibling" : "previousSibling";
+    let nextEnabledSibling: ChildNode = target[siblingKey];
+    if (nextEnabledSibling) {
+      while ((nextEnabledSibling as HTMLInputElement)?.disabled) {
+        if (!nextEnabledSibling) {
+          break;
+        }
+        nextEnabledSibling = nextEnabledSibling[siblingKey];
+      }
+      if ((nextEnabledSibling as HTMLInputElement)?.focus) {
+        (nextEnabledSibling as HTMLInputElement).focus();
+      }
+    }
+  }
+
+  trackBy(index: number): number {
+    return index;
   }
 
 }
---
+++ src/app/modules/entry/login/login-step-two/masked-password/masked-password.model.ts
@@ -0,0 +1,4 @@
+export interface MaskedPasswordChar {
+  disabled: boolean;
+  value: string;
+}
