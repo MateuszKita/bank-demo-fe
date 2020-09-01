a4bc81d12630395939ef72cb77da7f1ae45f9245
MateuszKita <mkita.zg@gmail.com>
2020-09-01 22:43:33 +0200

    don't lose login on go to previous step

--- src/app/modules/entry/login/login-step-one/login-step-one.component.ts
+++ src/app/modules/entry/login/login-step-one/login-step-one.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'bd-login-step-one',
@@ -9,7 +9,7 @@ export class LoginStepOneComponent implements OnInit {
 
   @Output() next: EventEmitter<string> = new EventEmitter<string>();
 
-  public login: string;
+  @Input() login: string;
 
   constructor() {
   }
