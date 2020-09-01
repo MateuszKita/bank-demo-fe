fe3847f2668d70ddf6ec42274127f0568c5092b5
MateuszKita <mkita.zg@gmail.com>
2020-09-01 19:07:40 +0200

    use bulma-divider, create simple layout for entry component

--- src/app/modules/entry/entry-routing.module.ts
+++ src/app/modules/entry/entry-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
+import {EntryComponent} from "./entry.component";
 
 const routes: Routes = [
   {
     path: '',
+    component: EntryComponent,
     children: [
       {
         path: 'login',
