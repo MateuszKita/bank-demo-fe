0150a1604ab0f7f3816c5201cdab627c69821a39
MateuszKita <mkita.zg@gmail.com>
2020-09-02 01:01:20 +0200

    create form for step two in registration

--- src/app/modules/entry/register/register.component.ts
+++ src/app/modules/entry/register/register.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {group} from "@angular/animations";
 
 @Component({
   selector: 'bd-register',
