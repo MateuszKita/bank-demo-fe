bb5408f5c211d6f7e678a0f6d94b24393fd75a50
MateuszKita <mkita.zg@gmail.com>
2020-09-01 17:19:30 +0200

    create components for entry module - login, register and advertisement

---
+++ src/app/modules/entry/advertisement/advertisement.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'bd-advertisement',
+  templateUrl: './advertisement.component.html',
+  styleUrls: ['./advertisement.component.scss']
+})
+export class AdvertisementComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/entry/entry.module.ts
+++ src/app/modules/entry/entry.module.ts
@@ -2,9 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {EntryRoutingModule} from './entry-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AdvertisementComponent } from './advertisement/advertisement.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, RegisterComponent, AdvertisementComponent],
   imports: [
     CommonModule,
     EntryRoutingModule
