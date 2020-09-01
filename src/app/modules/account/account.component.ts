f341f64041176699a5d429a950d3ab662acc5033
MateuszKita <mkita.zg@gmail.com>
2020-09-01 17:59:33 +0200

    add lazy loading, routing and refactor structure

---
+++ src/app/modules/account/account.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'bd-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.scss']
+})
+export class AccountComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/entry/entry.module.ts
+++ src/app/modules/entry/entry.module.ts
@@ -2,12 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {EntryRoutingModule} from './entry-routing.module';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { AdvertisementComponent } from './advertisement/advertisement.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {AdvertisementComponent} from './advertisement/advertisement.component';
+import {EntryComponent} from './entry.component';
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, AdvertisementComponent],
+  declarations: [LoginComponent, RegisterComponent, AdvertisementComponent, EntryComponent],
   imports: [
     CommonModule,
     EntryRoutingModule
