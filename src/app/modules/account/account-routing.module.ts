bee564b8735cd5465624914b93f999e11accdbe9
MateuszKita <mkita.zg@gmail.com>
2020-09-02 07:49:36 +0200

    update package json - add ngx-translate packages

--- src/app/modules/account/account-routing.module.ts
+++ src/app/modules/account/account-routing.module.ts
@@ -1,7 +1,34 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {AccountComponent} from "./views/account.component";
+import {AccountDashboardComponent} from "./views/account-dashboard/account-dashboard.component";
+import {AccountTransferComponent} from "./views/account-transfer/account-transfer.component";
+import {AccountHistoryComponent} from "./views/account-history/account-history.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AccountComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: AccountDashboardComponent
+      },
+      {
+        path: 'transfer',
+        component: AccountTransferComponent
+      },
+      {
+        path: 'history',
+        component: AccountHistoryComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'dashboard'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/account/account.module.ts
+++ src/app/modules/account/account.module.ts
@@ -3,9 +3,12 @@ import {CommonModule} from '@angular/common';
 
 import {AccountRoutingModule} from './account-routing.module';
 import { AccountComponent } from './views/account.component';
+import { AccountDashboardComponent } from './views/account-dashboard/account-dashboard.component';
+import { AccountTransferComponent } from './views/account-transfer/account-transfer.component';
+import { AccountHistoryComponent } from './views/account-history/account-history.component';
 
 @NgModule({
-  declarations: [AccountComponent],
+  declarations: [AccountComponent, AccountDashboardComponent, AccountTransferComponent, AccountHistoryComponent],
   imports: [
     CommonModule,
     AccountRoutingModule
